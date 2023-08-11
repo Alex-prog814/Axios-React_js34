3af06a9b923a23825f9095c458c2bcf9f5407818
Alex-prog814 <aleks.98.x.28@gmail.com>
2023-08-11 11:07:45 +0600

    Added something

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
   const API = 'http://localhost:8000/products';
 
   const [products, setProducts] = useState([]);
+  const [oneProduct, setOneProduct] = useState(null);
 
   function addProduct(newProduct) {
     axios.post(API, newProduct);
@@ -22,13 +23,23 @@ const App = () => {
     setProducts(res.data);
   };
 
+  async function getOneProduct(id) {
+    let res = await axios.get(`${API}/${id}`);
+    setOneProduct(res.data);
+  };
+
+  async function updateProduct(id, editedProduct) {
+    await axios.patch(`${API}/${id}`, editedProduct);
+    getProducts();
+  };
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<ProductList getProducts={getProducts} products={products} />} />
         <Route path="/add" element={<AddForm addProduct={addProduct} />} />
-        <Route path="/edit/:id" element={<EditForm />} />
+        <Route path="/edit/:id" element={<EditForm getOneProduct={getOneProduct} oneProduct={oneProduct} updateProduct={updateProduct} />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="/contacts" element={<h1>Contacts</h1>} />
       </Routes>
--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-const EditForm = () => {
+const EditForm = ({ getOneProduct, oneProduct, updateProduct }) => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const { id } = useParams();
+
+  useEffect(() => {
+    getOneProduct(id);
+  }, []);
+
+  useEffect(() => {
+    if(oneProduct) {
+      setTitle(oneProduct.title);
+      setPrice(oneProduct.price);
+      setImage(oneProduct.image);
+    };
+  }, [oneProduct, ]);
+
+  function saveChanges() {
+    let editedProduct = {
+      title,
+      price,
+      image
+    };
+    updateProduct(id, editedProduct);
+  };
+
   return (
-    <div>EditForm</div>
+    <div>
+      <input type="text" placeholder="Title" onChange={e => setTitle(e.target.value)} value={title} />
+      <input type="number" placeholder="Price" onChange={e => setPrice(e.target.value)} value={price} />
+      <input type="url" placeholder="Image" onChange={e => setImage(e.target.value)} value={image} />
+
+      <Link to="/">
+        <button onClick={saveChanges}>Save</button>
+      </Link>
+    </div>
   )
 }
 
